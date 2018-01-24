95bda11b5079fe7486720e3d6a445c5915f8e18e
Selamet Şamlı <selamet96@gmail.com>
2018-01-24 17:23:59 +0300

    splashActivity

--- Android/BurcRehberi/app/src/main/java/com/selametsamli/burcrehberi/MainActivity.kt
+++ Android/BurcRehberi/app/src/main/java/com/selametsamli/burcrehberi/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        setContentView(R.layout.activity_main)
+        window.statusBarColor=resources.getColor(R.color.statusBarColor)
 
 
         veriKaynagiHazirla()
